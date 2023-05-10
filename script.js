5c34abebcdd4a8fe43aa82cb940663d0fc80285f
Sandro Sousa Rivera <sandrosousarivera@sousas-P-machine.local>
2023-05-10 12:40:07 +0200

    full functionality

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const navigation= document.querySelector('.navigation')
 menuToggle.onclick= function(){
     navigation.classList.toggle('open')
 }
+
+const list = document.querySelectorAll('.list')
+function activarLink(){
+    list.forEach((item)=>
+    item.classList.remove('active'))
+    this.classList.add('active')
+}
+list.forEach((item)=>
+item.addEventListener('click', activarLink)
+)
